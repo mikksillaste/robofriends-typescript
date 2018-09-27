210377f5099b7474a02734245ece46e11854d5a7
mikksillaste <mikk.sillaste@gmail.com>
2018-09-27 07:57:17 +0300

    Initial commit

---
+++ src/components/Scroll.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+//Scroll components could render JSX element
+type Props = {
+  children?: JSX.Element
+}
+
+const Scroll = (props: Props) => {
+  return (
+    <div style={{ overflow: 'scroll', border: '5px solid black', height: '800px'}}>
+      {props.children}
+    </div>
+  );
+};
+
+export default Scroll;
